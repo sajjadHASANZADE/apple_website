c35b085e6bf7bdfcb5e35ad3629cbd1e08f5e72b
Lavenderer1375 <abedikianush@gmail.com>
2024-12-29 21:50:28 +0330

    New FS

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Footer from "./components/Footer";
+import Footer from "./components/main/Footer";
 import Hero from "./components/Hero";
 import Highlights from "./components/highlights/Highlights";
 import Model from "./components/iPhoneModel/Model";
