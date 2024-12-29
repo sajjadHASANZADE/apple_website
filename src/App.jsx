c2f156e763b4105be45178974f39d5f675be9fc0
Lavenderer1375 <abedikianush@gmail.com>
2024-12-29 20:06:13 +0330

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Highlights from "./components/highlights/Highlights";
 import Model from "./components/iPhoneModel/Model";
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
+      <Footer />
     </main>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const Footer = () => {
+  useGSAP(() => {
+    gsap.to("#footer", {
+      opacity: 1,
+      duration: 1,
+      delay: 0.5,
+      ease: "power2.inOut",
+    });
+  }, []);
+  return (
+    <section className="flex justify-center items-center bg-gray-300 text-2xl common-padding">
+      <div
+        id="footer"
+        className="bg-gray-100 opacity-0 rounded-lg text-black common-padding"
+      >
+        <h3>Build with love, by:</h3>
+        <code aria-disabled={true}>
+          <p>Kianoush Abedi</p>
+        </code>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
